e5d2b4d272897e76cacb49f5970cfd6a3508cc57
Nur Bar <LCD344@yahoo.com>
2018-07-30 14:04:44 +0100

    removed bottom scrollbar from marker view

--- src/components/global/ViewMarker.vue
+++ src/components/global/ViewMarker.vue
@@ -20,12 +20,12 @@
                        :path="marker.media.path" @instagram-loaded="$bus.$emit('fix-modal')"/>
             <template slot="content">
                 <div class="columns">
-                    <div class="column is-11">
+                    <div class="column is-10">
                         <div class="content">
                             <p v-text="marker.description"></p>
                         </div>
                     </div>
-                    <div class="column">
+                    <div class="column has-text-centered">
                         <button class="button is-dark is-rounded" @click="copyLink">
                             <span class="icon">
                                 <font-awesome-icon icon="copy"/>
