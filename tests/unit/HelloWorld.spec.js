810d84512375b7ecd1d2a410066e38f6e7f07215
Nur Bar <LCD344@yahoo.com>
2018-08-17 02:10:36 +0700

    installing mocha-chai-webpack

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -1,12 +1,10 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from '@vue/test-utils';
+import EditLayout from '@/views/EditLayout.vue';
 
-describe('HelloWorld.vue', () => {
+describe('EditLayout.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
+    const msg = 'new message';
+    const wrapper = shallowMount(EditLayout);
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
