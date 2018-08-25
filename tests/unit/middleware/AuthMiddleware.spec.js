666618cd80491c5f8476aef0b12baaf9c2bf7daa
Nur Bar <LCD344@yahoo.com>
2018-08-25 18:39:35 +0700

    added unit testing to authMiddleware

---
+++ tests/unit/middleware/AuthMiddleware.spec.js
@@ -0,0 +1,36 @@
+import sinon from 'sinon';
+import { assert } from 'chai';
+import auth from '@/services/authentication.service';
+import authMiddleware from '@/middleware/Auth.middleware';
+
+describe('Authentication Middleware', () => {
+
+	afterEach(() => {
+		sinon.restore();
+	});
+
+	it('Allows passage for logged in user',() => {
+		sinon.stub(auth,'isLoggedIn').callsFake(() => {
+			return true;
+		});
+		const nextSpy = sinon.spy();
+
+		authMiddleware.handle({},{}, nextSpy);
+
+		assert.isTrue(nextSpy.calledOnce);
+		assert.isTrue(nextSpy.calledWith());
+	});
+
+
+	it('Sends not logged in user to home',() => {
+		sinon.stub(auth,'isLoggedIn').callsFake(() => {
+			return false;
+		});
+		const nextSpy = sinon.spy();
+
+		authMiddleware.handle({},{}, nextSpy);
+
+		assert.isTrue(nextSpy.calledOnce);
+		assert.isTrue(nextSpy.calledWith('/'));
+	});
+});
