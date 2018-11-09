b7bb88e5358a3e7af0c6634796d9d9bc37e23b3a
Nur Bar <LCD344@yahoo.com>
2018-11-09 14:24:38 -0800

    fixed date displays for when date or day is less than 10

--- src/Components/edit/createMarker/DateTimeField.vue
+++ src/Components/edit/createMarker/DateTimeField.vue
@@ -37,7 +37,18 @@
 			getCurrentDate() {
 				const date = new Date();
 
-				return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+				let day = date.getDate();
+				if (day < 10) {
+					day = `0${day}`;
+				}
+
+				let month = date.getMonth() + 1;
+				if (month < 10) {
+					month = `0${month}`;
+				}
+
+
+				return `${date.getFullYear()}-${month}-${day}`;
 			},
 
 			getCurrentTime() {
