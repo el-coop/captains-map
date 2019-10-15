2cf906cbc8b6cae5e80c8f71a40529e2ba483724
Nur Bar <LCD344@yahoo.com>
2019-10-15 01:49:34 -0300

    added console to check something

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,6 +4,7 @@ import { register } from 'register-service-worker'
 import toast from 'izitoast';
 import store from "@/store";
 
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
 	register(`${process.env.BASE_URL}service-worker.js`, {
 		ready(registration) {
