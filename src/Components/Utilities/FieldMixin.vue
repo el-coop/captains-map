d596385eab150e41b17fe5e9a2ef8cde8d6507f1
Nur Bar <LCD344@yahoo.com>
2023-01-18 15:45:57 -0300

    missing import

--- src/Components/Utilities/FieldMixin.vue
+++ src/Components/Utilities/FieldMixin.vue
@@ -1,43 +1,45 @@
 <script>
-	export default {
-		name: "FieldMixin",
-        emits: ['update:modelValue'],
-        props: {
-			modelValue: {
-				default: ''
-			},
-			error: {
-				type: String,
-				default: ''
-			},
-			label: {
-				type: String,
-				default: ''
-			},
-			placeholder: {
-				type: String,
-				default: ''
-			},
-			name: {
-				type: String,
-				default: ''
-			},
-		},
-		data() {
-			return {
-                id: `label_${getCurrentInstance().uid}`
-			}
-		},
+import {getCurrentInstance} from "vue";
 
-		computed: {
-			val: {
-				get() {
-					return this.modelValue;
-				},
-				set(value) {
-					this.$emit('update:modelValue', value);
-				}
-			}
-		}
-	}
+export default {
+    name: "FieldMixin",
+    emits: ['update:modelValue'],
+    props: {
+        modelValue: {
+            default: ''
+        },
+        error: {
+            type: String,
+            default: ''
+        },
+        label: {
+            type: String,
+            default: ''
+        },
+        placeholder: {
+            type: String,
+            default: ''
+        },
+        name: {
+            type: String,
+            default: ''
+        },
+    },
+    data() {
+        return {
+            id: `label_${getCurrentInstance().uid}`
+        }
+    },
+    
+    computed: {
+        val: {
+            get() {
+                return this.modelValue;
+            },
+            set(value) {
+                this.$emit('update:modelValue', value);
+            }
+        }
+    }
+}
 </script>
