b7a81cb57bc12b9f43e6448bce1304ff070b8509
Nur Bar <LCD344@yahoo.com>
2023-05-09 15:33:17 +0300

    removed console log

--- tests/unit/Components/Map/Markers/MapUploadMarker.spec.js
+++ tests/unit/Components/Map/Markers/MapUploadMarker.spec.js
@@ -127,7 +127,6 @@ describe('MapUploadMarker.vue', () => {
 				marker
 			},
 		});
-		console.log(wrapper.html());
 		expect(wrapper.find('img[src="/api/marker/instagram/p/path"]').exists()).toBeTruthy();
 	});
 
